3957abb3d6849b194aacb9fa803327f499c0b682
Hudson Couto <hudson.sama@gmail.com>
2018-04-08 16:39:58 -0300

    create default dns_records after create a hosted_zone

--- lib/domain_service.rb
+++ lib/domain_service.rb
@@ -41,12 +41,15 @@ class DomainService
 
   def run
     STDERR.puts("[DomainService]#{call_id} - Processing #{input_action}")
+
     case input_action
     when 'create_hosted_zone'
       if not verify_hosted_zone
         create_dns
+        create_default_records
       else
         updated_dns(verify_hosted_zone)
+        create_default_records
       end
     end
   end
@@ -70,24 +73,35 @@ class DomainService
     }
   end
 
+  def create_default_records
+    default_records_1 = default_records_template(dns_hosted_zone_id['hosted_zone_id'], domain_name, 'A', values: ENV['AWS_ROUTE_IP'], comments: 'autocreated')
+
+    default_records_2 = default_records_template(dns_hosted_zone_id['hosted_zone_id'], "*.#{domain_name}", 'A', values: ENV['AWS_ROUTE_IP'], comments: 'autocreated')
+
+    route53.change_resource_record_sets(default_records_1)
+    route53.change_resource_record_sets(default_records_2)
+  end
+
   private
   def community
     @community ||= @pgconn.exec_params('select * from communities where id = $1', [community_id]).first
   end
 
   def verify_hosted_zone
-    dns_hosted_zone_id = @pgconn.exec_params(%Q{
-     select
-       response -> 'hosted_zone' ->> 'id' AS "hosted_zone_id"
-     from dns_hosted_zones
-     where id = $1;
-     }, [dns_hosted_zone['id']]).first
-
     if dns_hosted_zone_id['hosted_zone_id'].nil?
       false
     else
       response = route53.get_hosted_zone({ id: dns_hosted_zone_id['hosted_zone_id'] })
     end
+  end
+
+  def dns_hosted_zone_id
+    @dns_hosted_zone_id = @pgconn.exec_params(%Q{
+     select
+       response -> 'hosted_zone' ->> 'id' AS "hosted_zone_id"
+     from dns_hosted_zones
+     where id = $1;
+    }, [dns_hosted_zone['id']]).first
   end
 
   def dns_hosted_zone
@@ -116,6 +130,29 @@ class DomainService
         private_zone: false
       }
     }
+  end
+
+  def default_records_template hosted_zone_id, domain_name, type, values: nil, comments: nil, action: 'UPSERT', ttl_seconds: 300# 3600
+    batch = {
+      change_batch: {
+        changes: [
+          {
+            action: action,
+            resource_record_set: {
+              name: domain_name,
+              ttl: ttl_seconds,
+              type: type
+            },
+          }
+        ]
+      },
+      hosted_zone_id: hosted_zone_id
+    }
+
+    batch[:change_batch][:changes][0][:resource_record_set][:resource_records] = values.map{|v| { value: v } } if values
+    batch[:change_batch][:comment] = comments if comments
+
+    batch
   end
 
   def input_action
