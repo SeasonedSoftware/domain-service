3d1b7e0480d70bc97ef02a477551975cf109bca6
Hudson Couto <hudson.sama@gmail.com>
2018-04-11 18:20:12 -0300

    receiver stdin default format

--- func.rb
+++ func.rb
@@ -4,15 +4,17 @@ require 'jwt'
 require './lib/domain_service'
 
 def run(context, input)
-  if verify_token(input)
-    DomainService.run!(context, input)
+  puts input.inspect
+  if result = decode_token(input)
+    puts result.inspect
+    DomainService.run!(context, result[0])
   end
-rescue Exception => e
-  { error: "#{e.inspect}" }
+# rescue Exception => e
+#   { error: "#{e.inspect}" }
 end
 
-def verify_token(input)
-  JWT.decode(input['api_key'], ENV['JWT_SECRET'], true, algorithm: 'HS512')
+def decode_token(input)
+  JWT.decode(input, ENV['JWT_SECRET'], true, algorithm: 'HS512')
 end
 
 FDK.handle(:run)
